0ce78951f37c0006b5e98675287be9d446eabc4a
Aysan Isayo <aisayo@icloud.com>
2022-08-09 17:03:16 -0400

    starter code

--- 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
+++ 03_client_server_communication/flatiron_theater_company/app/controllers/productions_controller.rb
@@ -1,36 +1,41 @@
 class ProductionsController < ApplicationController
-  
-    def index 
+
+    def index
         render json: Production.all, status: :ok
-    end 
+    end
 
     def show
-         
         production = Production.find(params[:id])
         render json: production, status: :ok
-    end 
+        rescue ActiveRecord::RecordNotFound => error
+            render json: {message: error.message}
+    end
 
-    def create   
+    def create
         production = Production.create(production_params)
         render json: production, status: :created
-    end 
+    end
 
-    def update 
+    def update
         production = Production.find(params[:id])
         production.update(production_params)
         render json: production, status: :accepted
-    end 
+        rescue ActiveRecord::RecordNotFound => error
+            render json: {message: error.message}
+    end
 
     def destroy
         production = Production.find(params[:id])
         production.destroy
-        head :no_content 
-    end 
+        head :no_content
+        rescue ActiveRecord::RecordNotFound => error
+            render json: {message: error.message}
+    end
 
     private
 
     def production_params
         params.permit(:title, :genre, :description, :budget, :image, :director, :ongoing)
-    end 
+    end
 
 end
